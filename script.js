e75c55fb0b059b6b25127c7c3608254c7d9634e3
dudaz-dev <dudaz.dev@gmail.com>
2024-09-18 23:22:30 -0300

    adiciona função apagar um por vez

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function inserir(num) {
   document.querySelector(".resultado").innerHTML += num;
 }
 
-function clean() {
+function limpar() {
   document.querySelector(".resultado").innerHTML = "";
+}
+
+function volteUm() {
+  let tela = document.querySelector(".resultado").innerHTML;
+  document.querySelector(".resultado").innerHTML = tela.substring(0, tela.length - 1);
 }
